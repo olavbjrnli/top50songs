e45ab5483dd28e9628603013f999d7f319f78fee
olavbjrnli <olav_sp@hotmail.com>
2019-10-18 11:54:57 +0200

    Initial Commit

---
+++ Eksamen ordentlig/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Eksamen ordentlig
+//
+//  Created by Olav Backer-Grøndahl Bjørnli on 18/10/2019.
+//  Copyright © 2019 Olav Backer-Grøndahl Bjørnli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
